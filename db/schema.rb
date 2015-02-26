e66eed16384b88fb83f85c4269835b11ca1a972f
afinebojangle <rayford.taylor@gmail.com>
2015-02-25 21:04:56 -0500

    fixed ajax

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 20150212210256) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -50,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150212210256) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
